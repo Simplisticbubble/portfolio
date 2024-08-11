4414f35e400f3357f48783625b6159a5f510c97e
Simplisticbubble <ks.kevinseu@gmail.com>
2024-08-11 14:15:12 +1000

    added landing structure elements

--- client/portfolio/src/components/Landing.tsx
+++ client/portfolio/src/components/Landing.tsx
@@ -1,7 +1,17 @@
 function Landing(){
     return(
-        <div className="bg-[#E6CCB2] w-screen h-screen">
-            
+        <div className="bg-[#E6CCB2] w-screen flex items-center flex-col">
+            <div className="bg-[#EDE0D4] w-10/12 h-28 rounded-3xl flex justify-end items-center text-3xl font-roboto-mono px-5">
+                <p className=" mx-3">Home</p><p>|</p><p className="mx-3">About</p><p>|</p><p className="mx-3">Projects</p>
+            </div>
+            <div className="bg-[#9C6644] w-10/12 h-[40rem] rounded-3xl my-24">
+
+            </div>
+            <div className="w-10/12 h-1 rounded-2xl bg-[#EDE0D4]"></div>
+            <div className="bg-[#9C6644] w-10/12 h-[60rem] rounded-3xl my-24">
+
+            </div>
+            <div className="w-10/12 h-1 rounded-2xl bg-[#EDE0D4]"></div>
         </div>
     )
 }
